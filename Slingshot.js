d0653b1cec291ca92fbb21fe78843fc2349b88fc
Sarah <raghav.sarah@gmail.com>
2021-02-04 15:51:45 +0530

    c29

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.image1 = loadImage("sprites/sling1.png");
+        this.image2 = loadImage("sprites/sling2.png");
+        this.image3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -16,12 +19,28 @@ class SlingShot{
     }
 
     display(){
+        image(this.image1,200,20);
+        image(this.image2,170,20);
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48, 22, 8);
+            
+
+            if(pointA.x > 220){
+                strokeWeight(2);
+                line(pointA.x+20, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x+20,pointA.y, pointB.x+20, pointB.y);
+    
+                image(this.image3,pointA.x+25,pointA.y-15,15,30);
+            }else{
+                strokeWeight(4);
+                line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x-20,pointA.y, pointB.x+20, pointB.y);
+
+                image(this.image3,pointA.x-25,pointA.y-15,15,30);
+            }
         }
     }
-    
 }
